43410eb6539995a7273c86ca6f16370d9e7907d2
hebowei2000 <2312217486@qq.com>
2020-03-21 21:59:08 +0800

    some patches

--- Manderbot_set/Manderbot_set/main.cpp
+++ Manderbot_set/Manderbot_set/main.cpp
@@ -16,11 +16,11 @@ int main(int argc, char *argv[])
 {
 	int mp = 500;
 	double er = 2;
-	double rs = 0.001;
-	String  p = "God's fingerprint";
+	double rs = 1000;
+	String  p = "God's_fingerprint";
 	for (int i = 1;i<argc;i = i + 2)
 	{
-		if (argv[i][0] == '-'&&i + 1<argc)
+		if (argv[i][0] == '-'&& (i + 1) < argc)
 		{
 
 			if (argv[i][1] == 'm'&&argv[i][2] == 'p'&&argv[i][3] == '\0')
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 			else if (argv[i][1] == 'e'&&argv[i][2] == 'r'&&argv[i][3] == '\0')
 				er = atof(argv[i + 1]);
 			else if (argv[i][1] == 'r'&&argv[i][2] == 's'&&argv[i][3] == '\0')
-				rs = atof(argv[i + 1]);
+				rs = atoi(argv[i + 1]);
 			else if (argv[i][1] == 'p'&&argv[i][2] == '\0')
 				p = String(argv[i + 1]);
 			else
@@ -43,22 +43,23 @@ int main(int argc, char *argv[])
 			exit(1);
 		}
 	}
-	int length = ceil(4 / rs );
+	int length = 4 * rs;
+	double step = 1 / rs;
 	Mat  img = Mat::zeros(Size(length, length), CV_8UC3);
 	Mat  resized_img;
 	int colorfront[3] = { 255,255,255 };
 	int background[3] = { 0,0,255};
 	cout << "**************************************" << endl;
-	cout << "mp=" << mp << "\ner=" << er << "\nrs=" << rs << "\npath\"=" << p << ".jpg\"" << endl;
+	cout << "mp=" << mp << "\n er=" << er << "\n rs=" << rs << "\n path\"=" << p << ".jpg\"" << endl;
 	cout << "Threads:" << omp_get_max_threads() << endl << "Drawing........\n";
-	omp_set_num_threads(omp_get_max_threads() * 2);
+	omp_set_num_threads(omp_get_max_threads() * 2 );
 	for (int i = 0;i<length;i++)
 	{
 #pragma omp parallel for 
 		for (int j = 0;j<length;j++)
 		{
-			/* //use complex lib function
-			complex<double> c{-2+((double)i)*rs, -2+((double)j)*rs};
+			 //use complex lib function
+			complex<double> c{-2+((double)i)*step, -2+((double)j)*step};
 			complex<double> z{0,0};
 			int count=0;
 			while(count<=mp&&abs(z)<=er)
@@ -66,9 +67,9 @@ int main(int argc, char *argv[])
 			z=z*z+c;
 			count++;
 			}
-			*/
-			// reconstruct the code without complex lib to improve code efficiency
-			double ca = -2 + ((double)i)*rs;double cb = -2 + ((double)j)*rs;
+			
+			/*// reconstruct the code without complex lib to improve code efficiency
+			double ca = -2 + ((double)i)*step;double cb = -2 + ((double)j)*step;
 			double za = 0;double zb = 0;
 			double zaback;
 			int count = 0;
@@ -79,6 +80,7 @@ int main(int argc, char *argv[])
 				za = zaback;
 				count++;
 			}
+			*/
 			if (count>mp)
 			{
 				img.at<Vec3b>(j, i)[0] = background[0];
@@ -91,12 +93,13 @@ int main(int argc, char *argv[])
 				img.at<Vec3b>(j, i)[1] = colorfront[1];
 				img.at<Vec3b>(j, i)[2] = colorfront[2];
 			}
+			//cout << "Running_Threads:" << omp_get_num_threads() << endl;
 		}
 	}
 	
 	imwrite(p + ".jpg", img);
 	resize(img, resized_img, Size(img.cols / 5, img.rows / 5), 0, 0, INTER_LINEAR);
-	imshow("God's fingerprint", resized_img);
+	imshow(p+".jpg", resized_img);
 	waitKey(0);
 	return 0;
 }
